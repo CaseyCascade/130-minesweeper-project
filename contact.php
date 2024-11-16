d7e00492c5dc8f360582ac20de176b3701e6dc1a
Mattheau Casey <mattheauryan@gmail.com>
2024-11-15 19:09:08 -0800

    Added some random stuff to contact page

--- contact.php
+++ contact.php
@@ -7,5 +7,11 @@
         <?php
         include 'header.html';
         ?>
+
+    <h1>Contact</h1>
+    <h2>J.L. Bravo</h2>
+    <p>This is some text </p>
+    <h2>Mattheau Casey</h2>
+    <p>This is some text </p>
     </body>
 </html>
