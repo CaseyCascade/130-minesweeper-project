d4cac2f92bd421db62fdc955c671679aee0077f8
Mattheau Casey <mattheauryan@gmail.com>
2024-11-25 09:00:07 -0800

    added server info to process.php

---
+++ server/process.php
@@ -0,0 +1,10 @@
+<?php
+# Server Globals #TODO Idk how exactly this works without localhost so feel free to change the credentials 
+$servername = "localhost";
+$username = "caseycascade";
+$password = "Z7-X!8AOuK7*bnwr";
+$dbname = "minesweeper";
+
+function handleRequest() {}
+
+handleRequest();
