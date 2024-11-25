e74992a196bb90d9791e13287b370a807b9e8b30
Mattheau Casey <mattheauryan@gmail.com>
2024-11-25 12:13:04 -0800

    removed todo

--- server/process.php
+++ server/process.php
@@ -1,5 +1,5 @@
 <?php
-# Server Globals #TODO Idk how exactly this works without localhost so feel free to change the credentials 
+# Server Globals # Idk how exactly this works without localhost so feel free to change the credentials 
 $servername = "localhost";
 $username = "caseycascade";
 $password = "Z7-X!8AOuK7*bnwr";
