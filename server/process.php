543cb7a0c1431047a0b5477f9eb8c4cc4ce29e1e
Mattheau Casey <mattheauryan@gmail.com>
2024-11-25 12:12:13 -0800

    added some basic format for handle request code

--- server/process.php
+++ server/process.php
@@ -5,6 +5,46 @@ $username = "caseycascade";
 $password = "Z7-X!8AOuK7*bnwr";
 $dbname = "minesweeper";
 
-function handleRequest() {}
+function handleRequest() {
+    global $servername, $username, $password, $dbname, $data; 
 
-handleRequest();
+    // Establish a database connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check for connection errors
+    if ($conn->connect_error) {
+        return ["error" => "Connection failed: " . $conn->connect_error];
+    }
+
+    // Check if a specific key or parameter is set in the POST data
+    if (isset($_POST['action'])) {
+        $action = $_POST['action'];
+
+        // Handle different actions based on the 'action' key
+        switch ($action) {
+            case 'signup':
+                $username = isset($_POST['username']) ? $_POST['username'] : 0;
+                $password = isset($_POST['password']) ? $_POST['password'] : 0;
+                
+                // Code to register user into database //TODO
+                break;
+
+            case 'login':
+                $username = isset($_POST['username']) ? $_POST['username'] : 0;
+                $password = isset($_POST['password']) ? $_POST['password'] : 0;
+
+                // Code to login user //TODO
+                break; 
+                
+            default:
+                echo json_encode(['error' => 'Invalid action specified']);
+                break;
+        }
+    } else {
+        echo json_encode(['error' => 'No action specified']);
+    }
+
+    $conn->close();
+}
+
+handleRequest();  
