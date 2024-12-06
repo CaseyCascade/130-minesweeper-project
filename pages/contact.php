f84850480839a8fe1222d2f9e914ce6dc76779d7
Bravo <jluisbg10@gmail.com>
2024-12-05 23:16:33 -0800

    add contacts page

--- pages/contact.php
+++ pages/contact.php
@@ -6,16 +6,33 @@
     <?php
     include 'header.php';
     ?>
+    <link rel="stylesheet" href="../styles/contact.css">
 </head>
 <body>
-<main class="centered-thin">
-    
-    <h1>Contact</h1>
-    <h2>J.L. Bravo</h2>
-    <p>This is some text </p>
-    <h2>Mattheau Casey</h2>
-    <p>This is some text </p>
+<main>
+    <div class="contacts-container">
+        <div class="bio-card">
+            <img src="../data/contact/jlbravo.png" alt="J. L. Bravo" class="profile-image">
+            <h2>J. L. Bravo <span class="pronouns">(they/them)</span></h2>
+            <p class="description">
+            Hi, I'm Bravo! 
+            I made a game called <a href="https://store.steampowered.com/app/1626370/Raybound/">Raybound</a>, and I'm working on a sequel called <a href="https://www.youtube.com/watch?v=ZNIqfSJGSmg">Raybound: Subnovian Legend</a> :3
+            </p>
+            <p>
+            I worked a lot on the backend, php and mysql can be a headache sometimes :<
+            </p>
+            <a href="mailto:jluisbg10@gmail.com" class="email">jluisbg10@gmail.com</a>
+            <a href="mailto:jlbravo17@mail.fresnostate.edu" class="email">jlbravo17@mail.fresnostate.edu</a>
+            <br><br>
+        </div>
 
+        <div class="bio-card">
+            <img src="../data/contact/mcasey.jpg" alt="Mattheau Casey" class="profile-image">
+            <h2>Mattheau Casey <span class="pronouns">(he/him)</span></h2>
+            <p class="description">Mattheau Casey is a full-stack developer with expertise in cloud technologies and AI. He enjoys solving challenging problems and mentoring the next generation of coders.</p>
+            <a href="mailto:m.casey@example.com" class="email">m.casey@example.com</a>
+        </div>
+    </div>
 </main>
 <?php include 'footer.php' ?>
 </body>
