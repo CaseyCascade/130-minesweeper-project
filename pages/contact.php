f975f0056c24eb2e97790b9be8bcc8f3e8bab21b
Bravo <jluisbg10@gmail.com>
2024-12-06 00:01:07 -0800

    update contact

--- pages/contact.php
+++ pages/contact.php
@@ -16,14 +16,21 @@
             <h2>J. L. Bravo <span class="pronouns">(they/them)</span></h2>
             <p class="description">
             Hi, I'm Bravo! 
-            I made a game called <a href="https://store.steampowered.com/app/1626370/Raybound/">Raybound</a>, and I'm working on a sequel called <a href="https://www.youtube.com/watch?v=ZNIqfSJGSmg">Raybound: Subnovian Legend</a> :3
+            I made a game called <a href="https://store.steampowered.com/app/1626370/Raybound/">Raybound</a>, and I'm working on a sequel called <a href="https://www.youtube.com/watch?v=ZNIqfSJGSmg">Raybound: Subnovian Legend</a>!!!
             </p>
             <p class="description">
             I worked a lot on the backend, php and mysql can be a headache sometimes :<
             </p>
+            <p class="description">
+            Oh yeah, I also did the music for this very website, using LMMS :3
+            </p>
+            <div class="card-bottom">
             <a href="mailto:jluisbg10@gmail.com" class="email">jluisbg10@gmail.com</a>
             <a href="mailto:jlbravo17@mail.fresnostate.edu" class="email">jlbravo17@mail.fresnostate.edu</a>
-            <br><br>
+            <a href="https://github.com/braveo" class="github-link" target="_blank" aria-label="J. L. Bravo's GitHub">
+                <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" class="github-icon">
+            </a>
+            </div>
         </div>
 
         <div class="bio-card">
@@ -31,10 +38,15 @@
             <h2>Mattheau Casey <span class="pronouns">(he/him)</span></h2>
             <p class="description">
                 Hello! I'm Casey. As of Spring 2025 I'll be a graduate from Fresno State with a major in Computer Science and a Cybersecurity certification. I did a lot of the client side work and game logic on this website.</p>
-             <p class="description">   I'm a hobbyist game/software developer looking to expand my portolio soon. You can check out my <a href="https://github.com/CaseyCascade">github</a> here!
+             <p class="description">   I'm a hobbyist game/software developer looking to expand my portolio soon.
             </p>
+            <div class="card-bottom">
             <a href="mailto:mattheauryan@gmail.com" class="email">mattheauryan@gmail.com</a>
             <a href="mailto:caseycascade@mail.fresnostate.edu" class="email">caseycascade@mail.fresnostate.edu</a>
+            <a href="https://github.com/caseycascade" class="github-link" target="_blank" aria-label="Mattheau Casey's GitHub">
+                <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" class="github-icon">
+            </a>
+            </div>
         </div>
     </div>
 </main>
